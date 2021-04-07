7ee19b51aa4fa4a65a641ef28b74422c87a4e839
Jeremy Strauch <jtstrauch58@gmail.com>
2021-04-07 06:55:08 -0500

    basic table addition working

--- UFO-Level-1/js/app.js
+++ UFO-Level-1/js/app.js
@@ -11,22 +11,43 @@ for (i=0; i<headerKeys.length; i++) {
   var row = thead.append("th")
   row.text(headerKeys[i])
 }
+ 
+var tbody = d3.select("tbody");
+data.forEach(function(sitings) {
+      var row = tbody.append("tr");
+      Object.entries(sitings).forEach(function([key, value]) {
+      var cell = row.append("td");
+      cell.text(value);
+      });
+    });
 
-// Object.keys(data).forEach(function([key, value]) {
-//   var row = thead.append("th")
-//   console.log(row);
-//   row.text(key)
-//   console.log(key);
-// }
-// );
+var update = d3.select("#button-newUFO");
+var form = d3.select("#form-newUFO");
 
+update.on("click", appendUFOUpdate);
+form.on("submit", appendUFOUpdate);
 
-// 
-// var tbody = d3.select("tbody");
-// data.forEach(function(sitings) {
-//       var row = tbody.append("tr");
-//       Object.entries(sitings).forEach(function([key, value]) {
-//       var cell = row.append("td");
-//       cell.text(value);
-//       });
-//     });
+function appendUFOUpdate () {
+  d3.event.preventDefault();
+  console.log("An entry was made!");
+  var inputElement = d3.select("#text-newUFO");
+  var inputValue = inputElement.property("value");
+  var newData = inputValue.split(", ");
+  console.log(newData);
+  
+  var newEntry = {
+      datetime: newData[0],
+      city: newData[1],
+      state: newData[2],
+      country: newData[3],
+      shape: newData[4],
+      durationMinutes: newData[5],
+      comments: newData[6]
+    };
+    console.log(newEntry);
+  var tbody = d3.select("tbody");
+  var row = tbody.append("tr");
+  Object.entries(newEntry).forEach(function([key, value]) {
+    var cell = row.append("td");
+    cell.text(value);})
+};
