adff4eb6134b8bcf93fd9a7f3e0706f2b176c661
Jeremy Strauch <jtstrauch58@gmail.com>
2021-04-08 16:23:49 -0500

    adding css

--- UFO-Level-1/js/app.js
+++ UFO-Level-1/js/app.js
@@ -1,18 +1,22 @@
-// from data.js
+// import from data.js
 var tableData = data;
 
-// YOUR CODE HERE!
-
-// Use entered date range to find sightings
+// Select main table header
 var thead = d3.select("thead>tr");
+
+// determine array keys
 var headerKeys = Object.keys(tableData[0]);
 
+// append array keys to table header
 for (i=0; i<headerKeys.length; i++) {
   var row = thead.append("th")
   row.text(headerKeys[i])
 }
- 
+
+// select body of main table to insert data rows
 var tbody = d3.select("tbody");
+
+// insert table data using forEach
 tableData.forEach(function(sitings) {
       var row = tbody.append("tr");
       Object.entries(sitings).forEach(function([key, value]) {
@@ -21,11 +25,16 @@ tableData.forEach(function(sitings) {
       });
 });
 
+// set up event listener for button click and keyboard click
 var update = d3.select("#button-newUFO");
 var form = d3.select("#form-newUFO");
 
 update.on("click", appendUFOUpdate);
 form.on("submit", appendUFOUpdate);
+
+// append user input to the html table with new event
+// split the input string into array by splitting by ',' and space
+// send input to console for a second check
 
 function appendUFOUpdate () {
   d3.event.preventDefault();
@@ -34,7 +43,9 @@ function appendUFOUpdate () {
   var inputValue = inputElement.property("value");
   var newData = inputValue.split(", ");
   console.log(newData);
-  
+
+// create new array object and send to console
+
   var newEntry = {
       datetime: newData[0],
       city: newData[1],
@@ -45,6 +56,9 @@ function appendUFOUpdate () {
       comments: newData[6]
     };
     console.log(newEntry);
+
+// select main table and append the new data set
+
   var tbody = d3.select("tbody");
   var row = tbody.append("tr");
   Object.entries(newEntry).forEach(function([key, value]) {
@@ -52,11 +66,17 @@ function appendUFOUpdate () {
     cell.text(value);})
 };
 
+// set up search functionality
+// first set up listener group for button click and keyboard select
+
 var searchDate = d3.select("#button-dateSearch");
 var formDate = d3.select("#form-dateSearch");
 
 searchDate.on("click", searchUFODate);
 formDate.on("submit", searchUFODate);
+
+// set up function to select user search date
+// filter the data set for the specified date
 
 function searchUFODate () {
   d3.event.preventDefault();
@@ -70,14 +90,20 @@ function searchUFODate () {
   var ufoDateData = tableData.filter(searchDate);
   console.log(ufoDateData);
 
+// make new table to show filter results
+
   var thead = d3.select("#head-dateSearch");
   var tr = thead.append("tr");
   var headerKeys = Object.keys(tableData[0]);
+
+// add header
 
   for (i=0; i<headerKeys.length; i++) {
     var row = thead.append("th")
     row.text(headerKeys[i])
   };
+
+// add table data
 
   var tbody = d3.select("#body-dateSearch");
   console.log(tbody);
